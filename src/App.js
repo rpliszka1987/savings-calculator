b450e8bad299212dfee7ab714bd83bd20229abae
Robert Pliszka <robplisz@gmail.com>
2023-07-30 16:21:55 -0400

    Created resukts table to show savings results to user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/Header";
 import SavingsInput from "./Components/SavingsInput";
+import SavingsResults from "./Components/SavingsResutls";
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -32,33 +33,16 @@ function App() {
 
   return (
     <div>
+      {/* Page header component */}
       <Header />
 
+      {/* User input from */}
       <SavingsInput />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <SavingsResults />
     </div>
   );
 }
