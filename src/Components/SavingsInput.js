0dc818826df8a73790db6289784fd663358d4c14
Robert Pliszka <robplisz@gmail.com>
2023-07-30 17:37:25 -0400

    Created states for all the user input information

--- src/Components/SavingsInput.js
+++ src/Components/SavingsInput.js
@@ -9,7 +9,21 @@ function SavingsInput(props) {
   const [expectedReturn, setExpectedReturn] = useState("");
   const [duration, setDuration] = useState("");
 
-  
+  const currentSavingsHandler = (event) => {
+    setCurrentSavings(event.target.value);
+  };
+
+  const currentContributionHandler = (event) => {
+    setYearlyContribution(event.target.value);
+  };
+
+  const expectedReturnHandler = (event) => {
+    setExpectedReturn(event.target.value);
+  };
+
+  const durationHandler = (event) => {
+    setDuration(event.target.value);
+  };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -20,11 +34,21 @@ function SavingsInput(props) {
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={currentSavings}
+            onChange={currentSavingsHandler}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={yearlyContribution}
+            onChange={currentContributionHandler}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -32,11 +56,21 @@ function SavingsInput(props) {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={expectedReturn}
+            onChange={expectedReturnHandler}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={duration}
+            onChange={durationHandler}
+          />
         </p>
       </div>
       <p className="actions">
